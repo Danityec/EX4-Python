cc2b5d184ef748baf4b361d0b97737615fd3d5e5
Danityec <Danityec@gmail.com>
2021-01-26 16:27:51 +0200

    update 1

---
+++ task_1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import sklearn as skl
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
