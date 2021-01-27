031e4b92c1f4cdb0b860ff9b9b987b38014959ae
halpert-keren <halpertkeren@gmail.com>
2021-01-27 19:55:50 +0200

    data clean function

--- task1.py
+++ task1.py
@@ -113,10 +113,10 @@ def bayes_plot_with_failed_scatter(df):
         if y_pred_series[y_pred_series.index[i]] != y[y.index[i]]:
             failed_data.append(i)
 
-    new_data = pd.DataFrame(columns=['bill_length_mm', 'flipper_length_mm', 'species'])
+    new_data = pd.DataFrame(columns=['bill_length_mm', 'bill_depth_mm', 'species'])
     i = 0
     for f in failed_data:
-        new_data.loc[i] = ([x.loc[f]['bill_length_mm']] + [x.loc[f]['flipper_length_mm']] + [y_pred_series.loc[f]])
+        new_data.loc[i] = ([x.loc[f]['bill_length_mm']] + [x.loc[f]['bill_depth_mm']] + [y_pred_series.loc[f]])
         i += 1
 
     hue_order = clf.classes_
@@ -140,6 +140,7 @@ def bayes_classification_report(df):
 
     print(metrics.classification_report(y_test, y_pred))
 
+
 def prediction_naive_bayes(df):
 
     x = df.drop(["species"], axis=1)
@@ -161,48 +162,39 @@ def prediction_naive_bayes(df):
     plt.xticks(ticks=range(len(x.columns)), labels=x.columns, rotation=90)
     plt.show()
 
-
-
 def clean_data(df):
+    df['bill_length_mm'].fillna(round((df['bill_length_mm'].mean()), 2), inplace=True)
+    df['bill_depth_mm'].fillna(round((df['bill_depth_mm'].mean()), 2), inplace=True)
+    df['flipper_length_mm'].fillna(round((df['flipper_length_mm'].mean()), 2), inplace=True)
+    df['body_mass_g'].fillna(round((df['body_mass_g'].mean()), 2), inplace=True)
+    df['island'].fillna((df['island'].mode()[0]), inplace=True)
+    df['sex'].fillna((df['sex'].mode()[0]), inplace=True)
+    df['species'].fillna((df['species'].mode()[0]), inplace=True)
     df = categorical_to_numerical(df)
-    # species,island,bill_length_mm,bill_depth_mm,flipper_length_mm,body_mass_g,sex
-    df = df.where(df["sex_bin"] == "", 1)
-    df = df.where(df["species"] == "", 1)
-    df = df.where(df["island_bin"] == "", 1)
 
-    df = df.where(df["bill_length_mm"] == "", 1)
-    df = df.where(df["bill_depth_mm"] == "", 1)
-    df = df.where(df["flipper_length_mm"] == "", 1)
-
-    x_df = df.drop(['species'], axis=1)
-    y_df = df["species"]
-    print(x_df)
-    print(y_df)
-
-    return(x_df, y_df)
-
-
+    return df
 
 
 if __name__ == '__main__':
     penguins = pd.read_csv("penguins.csv")
+    penguins = clean_data(penguins)
 
-    x_penguins, y_penguins = clean_data(penguins)
     # 1.1:      --- we chose 'bill_length_mm' and 'flipper_length_mm'
-    # pair_plot_species(penguins)
+    pair_plot_species(penguins.drop(['sex_bin', 'island_bin'], axis=1))
 
     # 1.2:
     # x_penguins = penguins.drop(['species', 'island_bin', 'sex_bin', 'bill_depth_mm', 'body_mass_g'], axis=1)
-    # y_penguins = penguins['species']
+    x_penguins = penguins.drop(['species', 'island_bin', 'sex_bin', 'flipper_length_mm', 'body_mass_g'], axis=1)
+    y_penguins = penguins['species']
 
     gaussian_naive_bayes(x_penguins, y_penguins)
 
     # 1.3:
-    # bayes_plot(pd.concat([x_penguins, y_penguins], axis=1))
+    bayes_plot(pd.concat([x_penguins, y_penguins], axis=1))
 
     # 1.4:
-    # bayes_plot_with_failed_scatter(pd.concat([x_penguins, y_penguins], axis=1))
+    bayes_plot_with_failed_scatter(pd.concat([x_penguins, y_penguins], axis=1))
 
     # 1.5:
-    # bayes_classification_report(pd.concat([x_penguins, y_penguins], axis=1))
-    prediction_naive_bayes(penguins)
+    bayes_classification_report(pd.concat([x_penguins, y_penguins], axis=1))
+    # prediction_naive_bayes(penguins)
