e8356cfc034a2422946e3dc4c7bfe4590b2538cc
Danityec <Danityec@gmail.com>
2021-01-27 15:23:48 +0200

    updat test

--- task1.py
+++ task1.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn.inspection import permutation_importance
 
 
 def pair_plot_species(data):
@@ -139,25 +140,69 @@ def bayes_classification_report(df):
 
     print(metrics.classification_report(y_test, y_pred))
 
+def prediction_naive_bayes(df):
+
+    x = df.drop(["species"], axis=1)
+    y = df["species"]
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+
+    clf = GaussianNB()
+    clf = clf.fit(x_train, y_train)
+
+    result = permutation_importance(clf, x, y, n_repeats=10, random_state=0)
+    importance = zip(x.columns, result['importances_mean'])
+    # summarize feature importance
+    for i, v in importance:
+        print('Feature: %s, Score: %.5f' % (i, v))
+    # plot feature importance
+    print(len(x.columns), [x[1] for x in importance])
+    plt.bar(range(len(x.columns)), result['importances_mean'])
+    plt.xticks(ticks=range(len(x.columns)), labels=x.columns, rotation=90)
+    plt.show()
+
+
+
+def clean_data(df):
+    df = categorical_to_numerical(df)
+    # species,island,bill_length_mm,bill_depth_mm,flipper_length_mm,body_mass_g,sex
+    df = df.where(df["sex_bin"] == "", 1)
+    df = df.where(df["species"] == "", 1)
+    df = df.where(df["island_bin"] == "", 1)
+
+    df = df.where(df["bill_length_mm"] == "", 1)
+    df = df.where(df["bill_depth_mm"] == "", 1)
+    df = df.where(df["flipper_length_mm"] == "", 1)
+
+    x_df = df.drop(['species'], axis=1)
+    y_df = df["species"]
+    print(x_df)
+    print(y_df)
+
+    return(x_df, y_df)
+
+
+
 
 if __name__ == '__main__':
     penguins = pd.read_csv("penguins.csv")
-    penguins = penguins.dropna()
 
+    x_penguins, y_penguins = clean_data(penguins)
     # 1.1:      --- we chose 'bill_length_mm' and 'flipper_length_mm'
-    pair_plot_species(penguins)
+    # pair_plot_species(penguins)
 
     # 1.2:
-    penguins = categorical_to_numerical(penguins)
-    x_penguins = penguins.drop(['species', 'island_bin', 'sex_bin', 'bill_depth_mm', 'body_mass_g'], axis=1)
-    y_penguins = penguins['species']
+    # x_penguins = penguins.drop(['species', 'island_bin', 'sex_bin', 'bill_depth_mm', 'body_mass_g'], axis=1)
+    # y_penguins = penguins['species']
+
     gaussian_naive_bayes(x_penguins, y_penguins)
 
     # 1.3:
-    bayes_plot(pd.concat([x_penguins, y_penguins], axis=1))
+    # bayes_plot(pd.concat([x_penguins, y_penguins], axis=1))
 
     # 1.4:
-    bayes_plot_with_failed_scatter(pd.concat([x_penguins, y_penguins], axis=1))
+    # bayes_plot_with_failed_scatter(pd.concat([x_penguins, y_penguins], axis=1))
 
     # 1.5:
-    bayes_classification_report(pd.concat([x_penguins, y_penguins], axis=1))
+    # bayes_classification_report(pd.concat([x_penguins, y_penguins], axis=1))
+    prediction_naive_bayes(penguins)
