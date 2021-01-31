1b3c644a5a7df8a334fc03652dfc6d8cbf1bcbd1
Danityec <Danityec@gmail.com>
2021-01-31 11:02:37 +0200

    remove Task_1

--- task_1.py
+++ task_1.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn.inspection import permutation_importance
 
 
 def pair_plot_species(data):
@@ -29,7 +30,7 @@ def gaussian_naive_bayes(x_data, y_data):
     gnb_model.fit(x_train, y_train)
     y_model = gnb_model.predict(x_test)
 
-    #print(metrics.accuracy_score(y_test, y_model))
+    print(metrics.accuracy_score(y_test, y_model))
 
 
 def bayes_plot(df):
@@ -112,10 +113,10 @@ def bayes_plot_with_failed_scatter(df):
         if y_pred_series[y_pred_series.index[i]] != y[y.index[i]]:
             failed_data.append(i)
 
-    new_data = pd.DataFrame(columns=['bill_length_mm', 'flipper_length_mm', 'species'])
+    new_data = pd.DataFrame(columns=['bill_length_mm', 'bill_depth_mm', 'species'])
     i = 0
     for f in failed_data:
-        new_data.loc[i] = ([x.loc[f]['bill_length_mm']] + [x.loc[f]['flipper_length_mm']] + [y_pred_series.loc[f]])
+        new_data.loc[i] = ([x.loc[f]['bill_length_mm']] + [x.loc[f]['bill_depth_mm']] + [y_pred_series.loc[f]])
         i += 1
 
     hue_order = clf.classes_
@@ -140,24 +141,60 @@ def bayes_classification_report(df):
     print(metrics.classification_report(y_test, y_pred))
 
 
+def prediction_naive_bayes(df):
+
+    x = df.drop(["species"], axis=1)
+    y = df["species"]
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+
+    clf = GaussianNB()
+    clf = clf.fit(x_train, y_train)
+
+    result = permutation_importance(clf, x, y, n_repeats=10, random_state=0)
+    importance = zip(x.columns, result['importances_mean'])
+    # summarize feature importance
+    for i, v in importance:
+        print('Feature: %s, Score: %.5f' % (i, v))
+    # plot feature importance
+    print(len(x.columns), [x[1] for x in importance])
+    plt.bar(range(len(x.columns)), result['importances_mean'])
+    plt.xticks(ticks=range(len(x.columns)), labels=x.columns, rotation=90)
+    plt.show()
+
+def clean_data(df):
+    df['bill_length_mm'].fillna(round((df['bill_length_mm'].mean()), 2), inplace=True)
+    df['bill_depth_mm'].fillna(round((df['bill_depth_mm'].mean()), 2), inplace=True)
+    df['flipper_length_mm'].fillna(round((df['flipper_length_mm'].mean()), 2), inplace=True)
+    df['body_mass_g'].fillna(round((df['body_mass_g'].mean()), 2), inplace=True)
+    df['island'].fillna((df['island'].mode()[0]), inplace=True)
+    df['sex'].fillna((df['sex'].mode()[0]), inplace=True)
+    df['species'].fillna((df['species'].mode()[0]), inplace=True)
+    df = categorical_to_numerical(df)
+
+    return df
+
+
 if __name__ == '__main__':
     penguins = pd.read_csv("penguins.csv")
-    penguins = penguins.dropna()
+    penguins = clean_data(penguins)
 
     # 1.1:      --- we chose 'bill_length_mm' and 'flipper_length_mm'
-    pair_plot_species(penguins)
+    # pair_plot_species(penguins.drop(['sex_bin', 'island_bin'], axis=1))
 
     # 1.2:
-    penguins = categorical_to_numerical(penguins)
-    x_penguins = penguins.drop(['species', 'island_bin', 'sex_bin', 'bill_depth_mm', 'body_mass_g'], axis=1)
+    # x_penguins = penguins.drop(['species', 'island_bin', 'sex_bin', 'bill_depth_mm', 'body_mass_g'], axis=1)
+    x_penguins = penguins.drop(['species', 'island_bin', 'sex_bin', 'flipper_length_mm', 'body_mass_g'], axis=1)
     y_penguins = penguins['species']
+
     gaussian_naive_bayes(x_penguins, y_penguins)
 
     # 1.3:
     bayes_plot(pd.concat([x_penguins, y_penguins], axis=1))
 
     # 1.4:
-    bayes_plot_with_failed_scatter(pd.concat([x_penguins, y_penguins], axis=1))
+    # bayes_plot_with_failed_scatter(pd.concat([x_penguins, y_penguins], axis=1))
 
     # 1.5:
-    bayes_classification_report(pd.concat([x_penguins, y_penguins], axis=1))
+    # bayes_classification_report(pd.concat([x_penguins, y_penguins], axis=1))
+    # prediction_naive_bayes(penguins)
