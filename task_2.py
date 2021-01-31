04ff7bfebac60fae22ee234fb61351a2ce581178
Danityec <Danityec@gmail.com>
2021-01-31 20:13:28 +0200

    update task_2

--- task_2.py
+++ task_2.py
@@ -43,40 +43,47 @@ def gaussian_naive_bayes(x_data, y_data):
     #print(metrics.accuracy_score(y_test, y_model))
 
 
-def bayes_plot(df):
+def bayes_plot(df, spread=100):
     colors = 'seismic'
     col1 = df.columns[0]
     col2 = df.columns[1]
 
     y = df[df.columns[2]]
     x = df.drop(df.columns[2], axis=1)
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
-
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)  # 80% training and 20% test
     clf = GaussianNB()
     clf = clf.fit(x_train, y_train)
 
+    # Train Classifer
+
     prob = len(clf.classes_) == 2
+
+    # predict the response for test dataset
+
+    # hueorder = clf.classes_
+    #
+    # def numify(val):
+    #     return np.where(clf.classes_ == val)[0]
+
 
     x_min, x_max = x.loc[:, col1].min() - 1, x.loc[:, col1].max() + 1
     y_min, y_max = x.loc[:, col2].min() - 1, x.loc[:, col2].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.2), np.arange(y_min, y_max, 0.2))
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.2), np.arange(y_min, y_max, 0.2))    # הרזולוציה היא שגורמת למדרגות, היא מדורגת כי היא אינה אופטימלית
 
-    z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
+    z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])     # Probability of each decision , שיטוח הגריד
 
     if prob:
         z = z[:, 1] - z[:, 0]
     else:
         colors = "Set1"
-        z = np.argmax(z, axis=1)
+        z = np.argmax(z, axis=1)    # בחירת הארגומנט המקסימלי, כלומר Z הופך להיות ארגומנט של הסתברויות
 
-    z = z.reshape(xx.shape)
-
+    # Put the result into a color plot
+    z = z.reshape(xx.shape)            # החזרה לצורה של גריד
     plt.contourf(xx, yy, z, cmap=colors, alpha=0.5)
     plt.colorbar()
     if not prob:
         plt.clim(0, len(clf.classes_) + 3)
-
     fig = plt.gcf()
     fig.set_size_inches(12, 8)
     plt.show()
@@ -150,7 +157,8 @@ def bayes_plot_with_failed_scatter(df):
         if y_pred_series[y_pred_series.index[i]] != y[y.index[i]]:
             failed_data.append(i)
 
-    new_data = pd.DataFrame(columns=['bill_length_mm', 'bill_depth_mm', 'species'])
+
+    new_data = pd.DataFrame(columns=['bill_length_mm', 'bill_depth_mm', 'class'])
     i = 0
     for f in failed_data:
         new_data.loc[i] = ([x.loc[f]['bill_length_mm']] + [x.loc[f]['bill_depth_mm']] + [y_pred_series.loc[f]])
@@ -161,6 +169,7 @@ def bayes_plot_with_failed_scatter(df):
     sns.scatterplot(data=new_data, x=col1, y=col2, hue=df.columns[2], hue_order=hue_order, palette=colors)
     fig = plt.gcf()
     fig.set_size_inches(12, 8)
+
     plt.show()
 
 
@@ -181,10 +190,10 @@ if __name__ == '__main__':
     gaussian_naive_bayes(x_penguins, y_penguins)
 
     # 2.3:
-    # bayes_plot(pd.concat([x_penguins, y_penguins], axis=1))
+    bayes_plot(pd.concat([x_penguins, y_penguins], axis=1))
 
     # 2.4:
-    # bayes_plot_with_failed_scatter(pd.concat([x_penguins, y_penguins], axis=1))
+    bayes_plot_with_failed_scatter(pd.concat([x_penguins, y_penguins], axis=1))
 
     # 2.5:
     # bayes_classification_report(pd.concat([x_penguins, y_penguins], axis=1))
