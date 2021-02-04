921dae4bfd949aeaecc4841a3952b975cb0ba728
halpert-keren <halpertkeren@gmail.com>
2021-02-04 16:24:10 +0200

    code cleanup

--- task_1.py
+++ task_1.py
@@ -5,7 +5,6 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
-from sklearn.inspection import permutation_importance
 
 
 def pair_plot_species(data):
@@ -25,60 +24,32 @@ def categorical_to_numerical(data):
 
 def gaussian_naive_bayes(x_data, y_data):
     x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=1)
-
     gnb_model = GaussianNB()
     gnb_model.fit(x_train, y_train)
-    y_model = gnb_model.predict(x_test)
-
-    y_pred = pd.Series(y_model, name='prediction')
-    predicted = pd.concat([x_test.reset_index(), y_test.reset_index(), y_pred], axis=1)
-
-    predicted.to_csv('test.csv')
-    print(metrics.accuracy_score(y_test, y_model))
+    return gnb_model
 
 
 def bayes_plot(df):
-    colors = 'seismic'
-    col1 = df.columns[0]
-    col2 = df.columns[1]
-
     y = df[df.columns[2]]
     x = df.drop(df.columns[2], axis=1)
+    colors = "Set1"
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+    clf = gaussian_naive_bayes(x, y)
 
-    clf = GaussianNB()
-    clf = clf.fit(x_train, y_train)
-
-    prob = len(clf.classes_) == 2
-
-    x_min, x_max = x.loc[:, col1].min() - 1, x.loc[:, col1].max() + 1
-    y_min, y_max = x.loc[:, col2].min() - 1, x.loc[:, col2].max() + 1
+    x_min, x_max = x.loc[:, 'bill_length_mm'].min() - 1, x.loc[:, 'bill_length_mm'].max() + 1
+    y_min, y_max = x.loc[:, 'bill_depth_mm'].min() - 1, x.loc[:, 'bill_depth_mm'].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
 
     z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
-
-    if prob:
-        z = z[:, 1] - z[:, 0]
-    else:
-        colors = "Set1"
-        z = np.argmax(z, axis=1)
-
+    z = np.argmax(z, axis=1)
     z = z.reshape(xx.shape)
 
-    CP = plt.contourf(xx, yy, z, cmap=colors, alpha=0.5)
+    plt.contourf(xx, yy, z, cmap=colors, alpha=0.5)
     plt.colorbar()
-    if not prob:
-        plt.clim(0, len(clf.classes_) + 3)
-
-    # new_data = pd.DataFrame(columns=['bill_length_mm', 'bill_depth_mm', 'species'])
-    # new_data.loc[0] = ([] + [] + ['Adelie'])
-    # new_data.loc[1] = ([] + [] + ['Chinstrap'])
-    # new_data.loc[2] = ([] + [] + ['Gentoo'])
+    plt.clim(0, len(clf.classes_) + 3)
 
     plt.xlabel('bill_length_mm')
     plt.ylabel('bill_depth_mm')
-    # plt.legend(['Adelie', 'Chinstrap', 'Gentoo'])
     fig = plt.gcf()
     fig.set_size_inches(12, 8)
     plt.show()
@@ -87,50 +58,36 @@ def bayes_plot(df):
 def bayes_plot_with_failed_scatter(df):
     y = df[df.columns[2]]
     x = df.drop(df.columns[2], axis=1)
+    clf = gaussian_naive_bayes(x, y)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
-
-    clf = GaussianNB()
-    clf = clf.fit(x_train, y_train)
-
-    prob = len(clf.classes_) == 2
+    hue_order = clf.classes_
+    colors = "Set1"
 
     x_min, x_max = x.loc[:, 'bill_length_mm'].min() - 1, x.loc[:, 'bill_length_mm'].max() + 1
     y_min, y_max = x.loc[:, 'bill_depth_mm'].min() - 1, x.loc[:, 'bill_depth_mm'].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
 
     z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
-
-    if prob:
-        z = z[:, 1] - z[:, 0]
-    else:
-        colors = "Set1"
-        z = np.argmax(z, axis=1)
-
+    z = np.argmax(z, axis=1)
     z = z.reshape(xx.shape)
 
     plt.contourf(xx, yy, z, cmap=colors, alpha=0.5)
     plt.colorbar()
-    if not prob:
-        plt.clim(0, len(clf.classes_) + 3)
+    plt.clim(0, len(clf.classes_) + 3)
 
-    y_pred_full = clf.predict(x)
-    y_pred_series = pd.Series(y_pred_full)
+    y_pred_series = pd.Series(clf.predict(x))
     failed_data = []
     for i in range(len(y_pred_series)):
         if y_pred_series[y_pred_series.index[i]] != y[y.index[i]]:
             failed_data.append(i)
 
-    new_data = pd.DataFrame(columns=['bill_length_mm', 'bill_depth_mm', 'species'])
+    failed_df = pd.DataFrame(columns=['bill_length_mm', 'bill_depth_mm', 'species'])
     i = 0
     for f in failed_data:
-        new_data.loc[i] = ([x.loc[f]['bill_length_mm']] + [x.loc[f]['bill_depth_mm']] + [y.loc[f]])
+        failed_df.loc[i] = ([x.loc[f]['bill_length_mm']] + [x.loc[f]['bill_depth_mm']] + [y.loc[f]])
         i += 1
 
-    hue_order = clf.classes_
-    new_data = new_data.sort_values('species')
-    new_data2 = new_data.reset_index(drop=True)
-    sns.scatterplot(data=new_data2, x='bill_length_mm', y='bill_depth_mm', hue=new_data['species'], hue_order=hue_order, palette=colors)
+    sns.scatterplot(data=failed_df, x='bill_length_mm', y='bill_depth_mm', hue=failed_df['species'], hue_order=hue_order, palette=colors)
     fig = plt.gcf()
     fig.set_size_inches(12, 8)
     plt.show()
@@ -168,7 +125,7 @@ if __name__ == '__main__':
     penguins = clean_data(penguins)
 
     # 1.1:
-    # pair_plot_species(penguins.drop(['sex_bin', 'island_bin'], axis=1))
+    pair_plot_species(penguins.drop(['sex_bin', 'island_bin'], axis=1))
 
     # 1.2:
     x_penguins = penguins.drop(['species', 'island_bin', 'sex_bin', 'flipper_length_mm', 'body_mass_g'], axis=1)
@@ -183,4 +140,4 @@ if __name__ == '__main__':
     bayes_plot_with_failed_scatter(pd.concat([x_penguins, y_penguins], axis=1))
 
     # 1.5:
-    # bayes_classification_report(pd.concat([x_penguins, y_penguins], axis=1))
+    bayes_classification_report(pd.concat([x_penguins, y_penguins], axis=1))
--- task_2.py
+++ task_2.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
@@ -15,7 +14,6 @@ def new_colums_sexANDspecies(data):
     data["class"] = (df1 + ' ' + df2)
     data = data.drop(['species', 'sex'], axis=1)
 
-    #print(data)
     return data
 
 
@@ -26,7 +24,6 @@ def pair_plotClass(data):
 
 
 def categorical_to_numerical(data):
-
     island_list = ['Torgersen', 'Biscoe', 'Dream']
     data['island_bin'] = pd.Categorical(data.island, ordered=False, categories=island_list).codes
 
@@ -38,52 +35,28 @@ def gaussian_naive_bayes(x_data, y_data):
 
     gnb_model = GaussianNB()
     gnb_model.fit(x_train, y_train)
-    y_model = gnb_model.predict(x_test)
 
-    #print(metrics.accuracy_score(y_test, y_model))
+    return gnb_model
 
 
-def bayes_plot(df, spread=100):
-    colors = 'seismic'
-    col1 = df.columns[0]
-    col2 = df.columns[1]
-
+def bayes_plot(df):
     y = df[df.columns[2]]
     x = df.drop(df.columns[2], axis=1)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)  # 80% training and 20% test
-    clf = GaussianNB()
-    clf = clf.fit(x_train, y_train)
+    colors = "Set1"
 
-    # Train Classifer
+    clf = gaussian_naive_bayes(x, y)
 
-    prob = len(clf.classes_) == 2
+    x_min, x_max = x.loc[:, 'bill_length_mm'].min() - 1, x.loc[:, 'bill_length_mm'].max() + 1
+    y_min, y_max = x.loc[:, 'bill_depth_mm'].min() - 1, x.loc[:, 'bill_depth_mm'].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
 
-    # predict the response for test dataset
+    z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
+    z = np.argmax(z, axis=1)
+    z = z.reshape(xx.shape)
 
-    # hueorder = clf.classes_
-    #
-    # def numify(val):
-    #     return np.where(clf.classes_ == val)[0]
-
-
-    x_min, x_max = x.loc[:, col1].min() - 1, x.loc[:, col1].max() + 1
-    y_min, y_max = x.loc[:, col2].min() - 1, x.loc[:, col2].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))    # הרזולוציה היא שגורמת למדרגות, היא מדורגת כי היא אינה אופטימלית
-
-    z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])     # Probability of each decision , שיטוח הגריד
-
-    if prob:
-        z = z[:, 1] - z[:, 0]
-    else:
-        colors = "Set1"
-        z = np.argmax(z, axis=1)    # בחירת הארגומנט המקסימלי, כלומר Z הופך להיות ארגומנט של הסתברויות
-
-    # Put the result into a color plot
-    z = z.reshape(xx.shape)            # החזרה לצורה של גריד
     plt.contourf(xx, yy, z, cmap=colors, alpha=0.5)
     plt.colorbar()
-    if not prob:
-        plt.clim(0, len(clf.classes_) + 2)
+    plt.clim(0, len(clf.classes_) + 2)
     fig = plt.gcf()
     fig.set_size_inches(12, 8)
     plt.xlabel('bill_length_mm')
@@ -119,38 +92,24 @@ def bayes_classification_report(df):
 
 
 def bayes_plot_with_failed_scatter(df):
-    colors = 'seismic'
-    col1 = df.columns[0]
-    col2 = df.columns[1]
-
     y = df[df.columns[2]]
     x = df.drop(df.columns[2], axis=1)
+    clf = gaussian_naive_bayes(x, y)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+    colors = "Set1"
+    hue_order = clf.classes_
 
-    clf = GaussianNB()
-    clf = clf.fit(x_train, y_train)
-
-    prob = len(clf.classes_) == 2
-
-    x_min, x_max = x.loc[:, col1].min() - 1, x.loc[:, col1].max() + 1
-    y_min, y_max = x.loc[:, col2].min() - 1, x.loc[:, col2].max() + 1
+    x_min, x_max = x.loc[:, 'bill_length_mm'].min() - 1, x.loc[:, 'bill_length_mm'].max() + 1
+    y_min, y_max = x.loc[:, 'bill_depth_mm'].min() - 1, x.loc[:, 'bill_depth_mm'].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
 
     z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
-
-    if prob:
-        z = z[:, 1] - z[:, 0]
-    else:
-        colors = "Set1"
-        z = np.argmax(z, axis=1)
-
+    z = np.argmax(z, axis=1)
     z = z.reshape(xx.shape)
 
     plt.contourf(xx, yy, z, cmap=colors, alpha=0.5)
     plt.colorbar()
-    if not prob:
-        plt.clim(0, len(clf.classes_) + 2)
+    plt.clim(0, len(clf.classes_) + 2)
 
     y_pred_full = clf.predict(x)
     y_pred_series = pd.Series(y_pred_full)
@@ -166,10 +125,7 @@ def bayes_plot_with_failed_scatter(df):
         new_data.loc[i] = ([x.loc[f]['bill_length_mm']] + [x.loc[f]['bill_depth_mm']] + [y.loc[f]])
         i += 1
 
-    hue_order = clf.classes_
-    print(colors)
-
-    sns.scatterplot(data=new_data, x=col1, y=col2, hue=df.columns[2], hue_order=hue_order, palette=colors)
+    sns.scatterplot(data=new_data, x='bill_length_mm', y='bill_depth_mm', hue=df.columns[2], hue_order=hue_order, palette=colors)
     fig = plt.gcf()
     fig.set_size_inches(12, 8)
 
@@ -184,10 +140,9 @@ if __name__ == '__main__':
     penguins = new_colums_sexANDspecies(penguins)
 
     # 2.1:
-    # pair_plotClass(penguins)
+    pair_plotClass(penguins)
 
     # 2.2:
-    # ,island_bin,bill_length_mm,bill_depth_mm,flipper_length_mm,body_mass_g,class
     x_penguins = penguins.drop(['class', 'body_mass_g', 'island_bin', 'flipper_length_mm'], axis=1)
     y_penguins = penguins['class']
     gaussian_naive_bayes(x_penguins, y_penguins)
@@ -199,4 +154,4 @@ if __name__ == '__main__':
     bayes_plot_with_failed_scatter(pd.concat([x_penguins, y_penguins], axis=1))
 
     # 2.5:
-    # bayes_classification_report(pd.concat([x_penguins, y_penguins], axis=1))
+    bayes_classification_report(pd.concat([x_penguins, y_penguins], axis=1))
